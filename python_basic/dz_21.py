4f2b0a4c0181412306eff83bbd78f2f118b0c5d4
m.ruban <m.ruban@testportal.gov.ua>
2023-09-26 09:00:44 +0200

    hw 21

---
+++ python_basic/dz_21.py
@@ -0,0 +1,23 @@
+# Створити батьківський клас auto, який має атрибути:
+# brand, age, cоlor, mark і weight.
+# А також методи: move, birthday і stop.
+# Методи move і stop виводять повідомлення на екран «move» та «stop»,
+# а birthday збільшує атрибут age на 1.
+# Атрибути brand, age та mark є обов'язковими під час оголошення об'єкта.
+
+class Auto():
+    def __init__(self, brand, age, cоlor, mark, weight) -> None:
+        self.brand = brand,
+        self.age = age
+        self.color = cоlor
+        self.mark = mark
+        self.weight = weight
+
+    def move(self):
+        print('move')
+
+    def stop(self):
+        print('stop')
+
+    def birthday(self):
+        self.age += 1
