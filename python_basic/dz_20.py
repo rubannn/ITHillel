7bbc6b283cdd6b42a72d78373c9eb607027e07c0
m.ruban <m.ruban@testportal.gov.ua>
2023-09-26 08:41:51 +0200

    update dz_20

--- python_basic/dz_20.py
+++ python_basic/dz_20.py
@@ -17,13 +17,17 @@ import pyexcel as pe
 path = f'{os.getcwd()}/output/'
 csv_file = f'{path}data.csv'
 xlxs_file = f'{path}data.xlsx'
-headers = ['id', 'name', 'phone']
 
+newdata = [['id', 'name', 'phone']]
 with open(csv_file, newline='', encoding='utf-8') as f:
     reader = csv.DictReader(f)
-
-    sheet = pe.Sheet()
-    sheet.row += headers
     for row in reader:
-        sheet.row += [row['id'], row['name'], row['phone']]
-    sheet.save_as(xlxs_file)
+        newdata.append([row['id'], row['name'], row['phone']])
+    newdata = list(zip(*newdata))
+
+headers = [''] + [f'person {i+1:03d}' for i in range(len(newdata[0]) - 1)]
+sheet = pe.Sheet()
+sheet.row += headers
+for row in newdata:
+    sheet.row += [c for c in row]
+sheet.save_as(xlxs_file)
