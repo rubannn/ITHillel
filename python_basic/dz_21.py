bd602ebd0112e7ec48e58564c8dec4ab1f5f6f0f
m.ruban <m.ruban@testportal.gov.ua>
2023-09-26 10:36:25 +0200

    update hw 21 - 22

--- python_basic/dz_21.py
+++ python_basic/dz_21.py
@@ -6,8 +6,8 @@
 # Атрибути brand, age та mark є обов'язковими під час оголошення об'єкта.
 
 class Auto():
-    def __init__(self, brand, age, color, mark, weight) -> None:
-        self.brand = brand,
+    def __init__(self, brand, age, mark, color=None, weight=None) -> None:
+        self.brand = brand
         self.age = age
         self.color = color
         self.mark = mark
@@ -21,3 +21,10 @@ class Auto():
 
     def birthday(self):
         self.age += 1
+
+    def __str__(self) -> str:
+        return f'=={self.brand}== \n\t{self.age=} \n\t{self.mark=} \n\t{self.color=} \n\t{self.weight=}'
+
+
+auto = Auto('Ford', 10, 'New-13.8')
+print(auto)
--- python_basic/dz_22.py
+++ python_basic/dz_22.py
@@ -2,11 +2,11 @@ import time
 
 
 class Auto():
-    def __init__(self, brand, age, color, mark, weight) -> None:
-        self.brand = brand,
+    def __init__(self, brand, age, mark, color=None, weight=None) -> None:
+        self.brand = brand
         self.age = age
-        self.color = color
         self.mark = mark
+        self.color = color
         self.weight = weight
 
     def move(self):
@@ -18,11 +18,14 @@ class Auto():
     def birthday(self):
         self.age += 1
 
+    def __str__(self) -> str:
+        return f'=={self.brand}== \n\t{self.age=} \n\t{self.mark=} \n\t{self.color=} \n\t{self.weight=}'
+
 
 class Truck(Auto):
-    def __init__(self, brand, age, color, mark, weight) -> None:
-        super().__init__(brand, age, color, mark, weight)
-        # max_load
+    def __init__(self, brand, age, mark, max_load, color=None, weight=None) -> None:
+        super().__init__(brand, age, mark, color, weight)
+        self.max_load = max_load
 
     def move(self):
         super().move()
@@ -35,17 +38,24 @@ class Truck(Auto):
 
 
 class Car(Auto):
-    def __init__(self, brand, age, cоlor, mark, weight) -> None:
-        super().__init__(brand, age, cоlor, mark, weight)
-        # max_speed
+    def __init__(self, brand, age, mark, max_speed, color=None, weight=None) -> None:
+        super().__init__(brand, age, mark, color, weight)
+        self.max_speed = max_speed
 
     def move(self):
         super().move()
         print(f'max speed is {self.max_speed}')
 
 
-truck1 = Truck()
-truck2 = Truck()
+truck1 = Truck('Man', 5, 'm1', 5000)
+truck2 = Truck('KrAZ', 15, '03-p12', 15000, 'green', 5000)
+print(truck1, truck2, sep='\n')
 
-car1 = Car()
-car2 = Car()
+car1 = Car('Ford', 12, 'Focus', 180, 'red')
+car2 = Car('Mazda', 7, 'CX-5', 201, weight=1500)
+print(car1, car2, sep='\n')
+
+truck2.load()
+truck1.move()
+
+car2.move()
