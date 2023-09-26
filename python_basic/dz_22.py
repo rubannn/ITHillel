9f518763ed7bc2393c2bdbdd2e42e58555f27765
m.ruban <m.ruban@testportal.gov.ua>
2023-09-26 09:23:25 +0200

    dz_22 prepare

---
+++ python_basic/dz_22.py
@@ -0,0 +1,51 @@
+import time
+
+
+class Auto():
+    def __init__(self, brand, age, cоlor, mark, weight) -> None:
+        self.brand = brand,
+        self.age = age
+        self.color = cоlor
+        self.mark = mark
+        self.weight = weight
+
+    def move(self):
+        print('move')
+
+    def stop(self):
+        print('stop')
+
+    def birthday(self):
+        self.age += 1
+
+
+class Truck(Auto):
+    def __init__(self, brand, age, cоlor, mark, weight) -> None:
+        super().__init__(brand, age, cоlor, mark, weight)
+        # max_load
+
+    def move(self):
+        super().move()
+        print('attention')
+
+    def load(self):
+        time.sleep(1)
+        print('load')
+        time.sleep(1)
+
+
+class Car(Auto):
+    def __init__(self, brand, age, cоlor, mark, weight) -> None:
+        super().__init__(brand, age, cоlor, mark, weight)
+        # max_speed
+
+    def move(self):
+        super().move()
+        print(f'max speed is {self.max_speed}')
+
+
+truck1 = Truck()
+truck2 = Truck()
+
+car1 = Car()
+car2 = Car()
