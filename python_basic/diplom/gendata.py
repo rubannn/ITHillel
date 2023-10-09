e261a9aa476e63a3aaae8dd95d7441ff0a8a17a0
m.ruban <m.ruban@testportal.gov.ua>
2023-10-09 10:24:38 +0200

    diplom update

--- python_basic/diplom/gendata.py
+++ python_basic/diplom/gendata.py
@@ -12,7 +12,32 @@ def get_age(born, death):
         today = date.today()
     return today.year - born.year - ((today.month, today.day) < (born.month, born.day))
 
+
+def get_surname(fname):
+    sur_list = []
+    with open(f'{fname}', 'r', encoding='utf-8') as f:
+        for s in f.readlines():
+            sur_list.append(s.strip('\n'))
+    return sur_list
+
+
+def get_name(is_male, fake, surname_path):
+    surname_female = get_surname(f'{surname_path}ua_surname_f.txt')
+    surname_male = get_surname(f'{surname_path}ua_surname_m.txt')
+    name = []
+    while len(name) < 2:
+        if is_male:
+            name = fake.name_male()
+            surname = random.choice(surname_male)
+        else:
+            name = fake.name_female()
+            surname = random.choice(surname_female)
+        name = [nm for nm in name.split() if 'пан' not in nm]
+    return (name[1], name[0], surname)
+
+
 path = os.getcwd()
+surname_path = f'{path}/python_basic/diplom/'
 fake = Faker(['uk_UA'])
 
 sex = ["чоловік", "жінка"]
@@ -22,12 +47,10 @@ with open(f"{path}/output/diplom_data.csv", 'w', encoding='utf-8') as f:
     writer = csv.writer(f, delimiter=';', lineterminator='\n')
     for x in range(num):
         gender = random.choice(sex)
-        if gender == sex[0]:
-            name = fake.name_male()
-        else:
-            name = fake.name_female()
+        name = get_name(gender == sex[0], fake, surname_path)
+
         born = fake.date_of_birth(None, 20, 70)
         death = fake.date_between(born) if random.choice([True, False]) else None
-        name = [nm for nm in name.split() if 'пан' not in nm]
+
         csv_data = [*name, born, death, gender, get_age(born, death)]
         writer.writerow(csv_data)
